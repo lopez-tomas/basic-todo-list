aca5c1cecd1289074d4e145d7f124ea350594ef4
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-12 17:38:59 -0300

    [FEAT]: using AppContext and initialState set through useInitialState hook. Header, Form & ToDoList imported. Some styles added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import AppContext from './context/AppContext';
+import useInitialState from './hooks/useInitialState';
+import Header from './components/Header';
+import Form from './containers/Form';
+import ToDoList from './containers/ToDoList';
+import './styles/App.sass';
 
 function App() {
+  const initialState = useInitialState();
+
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <AppContext.Provider value={initialState}>
+      <Header />
+      <main className="App">
+        <section className="form-container">
+          <h1>Create your TO DOs!</h1>
+          <Form/>
+        </section>
+        <section className="todos-container">
+          <h1>TO DO List</h1>
+          <ToDoList />
+        </section>
+      </main>
+    </AppContext.Provider>
   );
 }
 
