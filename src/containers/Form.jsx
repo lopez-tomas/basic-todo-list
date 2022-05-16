fb7f99222bb04942c2439d61efedde5ae571cd65
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-15 23:35:18 -0300

    [FEAT]: AppContext used. createToDo() function used. CForm reference created.

--- src/containers/Form.jsx
+++ src/containers/Form.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import AppContext from '../context/AppContext';
 
 const Form = () => {
+  const { state, createToDo } = useContext(AppContext);
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    let todo = formData.get('todo');
+    formData.set('todo', "");
+
+    const data = {
+      id: state.lastId,
+      todo: todo,
+    }
+    if (todo !== "") {
+      createToDo(data);
+      form.current.reset();
+    }
+  }
+
   return (
-    <form className="Form">
+    <form action="/" className="Form" ref={form}>
       <input
         className="input todo-input"
         type="text"
         name="todo"
         placeholder="Buy apples"
+        required
       />
       <input
+        onClick={handleSubmit}
         className="btn todo-btn"
         type="submit"
         value="Create"
