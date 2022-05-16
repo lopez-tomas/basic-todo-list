f03748d71110df2a7987194f724afd2f17fcb86f
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-15 23:38:32 -0300

    [FEAT]: AppContext used. TO DOs displayed if they exist.

--- src/containers/ToDoList.jsx
+++ src/containers/ToDoList.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import ToDo from '../components/ToDo';
 
 const ToDoList = () => {
+  const { state } = useContext(AppContext);
+
   return (
-    <ul>
-      <ToDo />
-      <ToDo />
-      <ToDo />
-      <ToDo />
+    <ul className="ToDoList">
+      {state.todos.map(todo => (
+        <ToDo key={todo.id} todo={todo} />
+      ))}
     </ul>
   )
 }
