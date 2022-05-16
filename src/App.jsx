06dc7125f0e6d546dfbfc04bf7ba2df270a0d19c
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-15 21:20:34 -0300

    [FIX]: clean project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import AppContext from './context/AppContext';
-import useInitialState from './hooks/useInitialState';
-import Header from './components/Header';
-import Form from './containers/Form';
-import ToDoList from './containers/ToDoList';
-import Footer from './components/Footer';
-import './styles/App.sass';
 
 function App() {
-  const initialState = useInitialState();
-
   return (
-    <AppContext.Provider value={initialState}>
-      <Header />
-      <main className="App">
-        <section className="Form-container">
-          <h1>Create your TO DOs!</h1>
-          <Form/>
-        </section>
-        <section className="ToDoList-container">
-          <h1>TO DO List</h1>
-          <ToDoList />
-        </section>
-      </main>
-      <Footer />
-    </AppContext.Provider>
+    <main>
+      <h1>Hello, World!</h1>
+    </main>
   );
 }
 
