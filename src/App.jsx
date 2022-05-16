9df6bed927a82f33e737e3aec02f968792fbde34
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-15 21:50:49 -0300

    [FEAT]: styles; Form & ToDoList containers imported.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Form from './containers/Form';
+import ToDoList from './containers/ToDoList';
+import './styles/App.sass';
 
 function App() {
   return (
-    <main>
-      <h1>Hello, World!</h1>
+    <main className="App">
+      <section className="Form-container">
+        <h1>Create your TO DOs!</h1>
+        <Form />
+      </section>
+      <section className="ToDoList-container">
+        <h1>TO DO List</h1>
+        <ToDoList />
+      </section>
     </main>
   );
 }
