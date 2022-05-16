ae0b16968ca78c1c8ad24569c5d8e6223de8e9a8
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-16 00:43:32 -0300

    [FEAT]: New structure. Using completeAll() & uncompleteAll() functions with new handleStatus() function. New state added to change Button.

--- src/containers/ToDoList.jsx
+++ src/containers/ToDoList.jsx
@@ -1,16 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import AppContext from '../context/AppContext';
 import ToDo from '../components/ToDo';
 
 const ToDoList = () => {
-  const { state } = useContext(AppContext);
+  const [allCompleted, setAllCompleted] = useState(false);
+  const { state, completeAll, uncompleteAll } = useContext(AppContext);
+
+  const handleStatus = () => {
+    if (allCompleted) {
+      uncompleteAll()
+      setAllCompleted(false);
+    } else {
+      completeAll()
+      setAllCompleted(true);
+    }
+  }
 
   return (
-    <ul className="ToDoList">
-      {state.todos.map(todo => (
-        <ToDo key={todo.id} todo={todo} />
-      ))}
-    </ul>
+    <div className="ToDoList">
+      <ul className="ToDoList-list">
+        {state.todos.map(todo => (
+          <ToDo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+
+      <button onClick={() => handleStatus()}>{allCompleted ? 'Uncomplete all' : 'Complete all'}</button>
+    </div>
   )
 }
 
